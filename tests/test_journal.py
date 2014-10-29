1fe4bf7471d7f84ae6879d92c7ed969e24b01383
Matthias Kümmerer <matthias@matthias-k.org>
2014-10-29 14:24:33 +0100

    Better test
    Signed-off-by: Matthias Kümmerer <matthias@matthias-k.org>

--- tests/test_journal.py
+++ tests/test_journal.py
@@ -1,14 +1,21 @@
+import matplotlib
+matplotlib.use('Agg')
+
 from unittest import TestCase
 import os
+import glob
 import shutil
+
 import matplotlib.pyplot as plt
 
 import ipythonjournal
 
 class JournalTest(TestCase):
-    #def tearDown(self):
-    #    if os.path.exists('Journal_files'):
-    #        shutil.rmtree('Journal_files')
+    def tearDown(self):
+        if os.path.exists('Journal_files'):
+            shutil.rmtree('Journal_files')
     def test_a(self):
         plt.plot([0,1,0])
         ipythonjournal.savefig()
+        files = glob.glob(os.path.join('Journal_files', '*.png'))
+        self.assertEqual(len(files), 1)
