9814501a50eb783ae5da5f161d056f2307e0f8ca
Matthias Kümmerer <matthias@matthias-k.org>
2014-10-29 15:18:03 +0100

    Test nbconvert
    Signed-off-by: Matthias Kümmerer <matthias@matthias-k.org>

--- tests/test_journal.py
+++ tests/test_journal.py
@@ -5,17 +5,67 @@ from unittest import TestCase
 import os
 import glob
 import shutil
+import subprocess as sp
 
 import matplotlib.pyplot as plt
 
 import ipythonjournal
 
+notebook = """
+{
+ "metadata": {
+  "name": "Journal"
+ },
+ "nbformat": 3,
+ "nbformat_minor": 0,
+ "worksheets": [
+  {
+   "cells": [
+    {
+     "cell_type": "code",
+     "collapsed": false,
+     "input": [],
+     "language": "python",
+     "metadata": {},
+     "outputs": []
+    }
+   ],
+   "metadata": {}
+  }
+ ]
+}
+"""
+
 class JournalTest(TestCase):
+    def setUp(self):
+        os.makedirs('tmp_dir')
+
+    def assert_files_present(self, **kwargs):
+        for ext in kwargs:
+            files = glob.glob(os.path.join('Journal_files', '*.{0}'.format(ext)))
+            self.assertEqual(len(files), kwargs[ext])
+        if kwargs:
+            total_count = sum(kwargs.values())
+        else:
+            total_count = 0
+        all_files = glob.glob(os.path.join('Journal_files', '*'))
+        self.assertEqual(len(all_files), total_count)
+
+
     def tearDown(self):
         if os.path.exists('Journal_files'):
             shutil.rmtree('Journal_files')
-    def test_a(self):
-        plt.plot([0,1,0])
+        if os.path.exists('tmp_dir'):
+            shutil.rmtree('tmp_dir')
+
+    def test_savefig(self):
+        plt.plot([0, 1, 0])
         ipythonjournal.savefig()
-        files = glob.glob(os.path.join('Journal_files', '*.png'))
-        self.assertEqual(len(files), 1)
+        self.assert_files_present(png=1)
+
+    def test_nbconvert(self):
+        notebook_file = os.path.join('tmp_dir', 'notebook.ipynb')
+        open(notebook_file, 'w').write(notebook)
+        sp.check_call(['ipython-journal', notebook_file])
+        self.assert_files_present(html=1)
+
